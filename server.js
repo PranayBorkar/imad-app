4c80ab68ca98588e103a3904cb07d64c14e9bd4b
PranayBorkar <borkar.tanu@gmail.com>
2017-08-12 22:33:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleone',function(req, res){
+app.get('/article-one',function(req, res){
     
     res.send('article one is requested and server here')
 });
 
-app.get('/articleone',function(req, res){
+app.get('/article-two',function(req, res){
     
     res.send('article two is requested and server here')
 });
 
-app.get('/articleone',function(req, res){
+app.get('/article-three',function(req, res){
     
     res.send('article three is requested and server here')
 });
